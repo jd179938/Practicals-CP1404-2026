0318ba050bfb816fdce3929eca18a9517cffa79b
jd179938 <lucy.doolan@my.jcu.edu.au>
2026-07-14 09:59:20 +1000

    Basic set up for prac question 2

---
+++ prac_06/languages.py
@@ -0,0 +1,10 @@
+from prac_06.programming_language import ProgrammingLanguage
+
+def main():
+    java = ProgrammingLanguage("Static", True, 1995)
+    c_plus_plus = ProgrammingLanguage("Static", False, 1983)
+    python = ProgrammingLanguage("Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Static", False, 1991)
+    ruby = ProgrammingLanguage("Dynamic", True, 1995)
+
+main()
