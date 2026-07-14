753e441dcaabd63fc615fb1500a087e4caec4938
jd179938 <lucy.doolan@my.jcu.edu.au>
2026-07-14 10:04:54 +1000

    Add string method to print classes

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,10 +1,16 @@
 from prac_06.programming_language import ProgrammingLanguage
 
 def main():
-    java = ProgrammingLanguage("Static", True, 1995)
-    c_plus_plus = ProgrammingLanguage("Static", False, 1983)
-    python = ProgrammingLanguage("Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguage("Static", False, 1991)
-    ruby = ProgrammingLanguage("Dynamic", True, 1995)
+    java = ProgrammingLanguage("Java", "Static", True, 1995)
+    c_plus_plus = ProgrammingLanguage("C++", "Static", False, 1983)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+
+    print(java)
+    print(c_plus_plus)
+    print(python)
+    print(visual_basic)
+    print(ruby)
 
 main()
--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,8 +1,12 @@
 class ProgrammingLanguage:
-    def __init__(self, typing, reflection, year):
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
         return self.typing == "Static"
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} typing, Reflection={self.reflection}, First appeared in {self.year}"
