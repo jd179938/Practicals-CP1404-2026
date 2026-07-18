8dd8d444987adf6f7ad4110371836eebd14adb4a
jd179938 <lucy.doolan@my.jcu.edu.au>
2026-07-18 18:53:20 +1000

    Enable user to enter a new guitar and add it to file and object

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -2,22 +2,33 @@ from guitar import Guitar
 
 guitars = []
 
+
 def main():
+    guitars = open_file()
+    add_guitar(guitars)
+    guitars.sort()
+    for guitar in guitars:
+        print(guitar)
+
+
+def open_file():
     in_file = open("guitars.csv", "r")
     for line in in_file:
         parts = line.strip().split(",")
         guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
         guitars.append(guitar)
     in_file.close()
+    return guitars
 
-    for guitar in guitars:
-        print(guitar)
+def add_guitar(guitars):
+    name = input("Guitar name: ")
+    year = int(input("Year: "))
+    cost = float(input("Cost: "))
 
-    print("-------------------------------")
-
-    guitars.sort()
-    for guitar in guitars:
-        print(guitar)
+    new_guitar = Guitar(name, year, cost)
+    guitars.append(new_guitar)
+    out_file = open("guitars.csv", "a")
+    out_file.write(f"\n{str(new_guitar.name)},{str(new_guitar.year)},{str(new_guitar.cost)}")
+    out_file.close()
 
 main()
-
